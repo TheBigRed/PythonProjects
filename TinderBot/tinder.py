8dfb84fceb6324e017cfd960fd76aa6d55540d20
Unknown <ramraja.vii@gmail.com>
2018-02-23 02:25:55 -0500

    Create tinder api library
    *Add function to get tinder auth
    *Change namespace in config file
    *Test for success 200 in tinder.py

--- TinderBot/tinder.py
+++ TinderBot/tinder.py
@@ -1,8 +1,12 @@
 import sys
 import config
+import tinder_api
+
 
 def main(argv):
-    print(config.fb_user_id)
+
+    tinder_api.get_auth_token(config.fb_auth_token, config.fb_user_id)
+
 
 if __name__ == '__main__':
     main(sys.argv)
---
+++ TinderBot/tinder_api.py
@@ -0,0 +1,32 @@
+import json
+
+import config
+import requests
+
+headers = {
+    'app_version': '6.9.4',
+    'platform': 'ios',
+    "content-type": "application/json",
+    "User-agent": "Tinder/7.5.3 (iPhone; iOS 10.3.2; Scale/2.00)",
+}
+
+
+def get_auth_token(fb_auth_token, fb_user_id):
+    if "error" in fb_auth_token:
+        return {"error": "could not retrieve fb_auth_token"}
+    if "error" in fb_user_id:
+        return {"error": "could not retrieve fb_user_id"}
+    url = config.host + '/auth'
+    req = requests.post(url,
+                        headers=headers,
+                        data=json.dumps(
+                            {'facebook_token': fb_auth_token, 'facebook_id': fb_user_id})
+                        )
+    try:
+        tinder_auth_token = req.json()["token"]
+        headers.update({"X-Auth-Token": tinder_auth_token})
+        print("You have been successfully authorized!")
+        return tinder_auth_token
+    except Exception as e:
+        print(e)
+    return {"error": "Something went wrong. Sorry, but we could not authorize you."}
