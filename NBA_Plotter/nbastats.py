31e7ae65f6e6dbd1c40c3121733fff2d38cf1862
Unknown <ramraja.vii@gmail.com>
2018-03-07 01:56:07 -0500

    Create Function to Scrape NBA Stats
    *Set headers
    *Set endpoint
    *Set parameters
    *Import in to dataframe
    *Print dataframe

---
+++ NBA_Plotter/nbastats.py
@@ -0,0 +1,71 @@
+import pandas
+import requests
+import json
+import os
+
+
+def main():
+    df = scrapeURL()
+    df.head()
+    print(df)
+
+
+def scrapeURL():
+    user_agent_headers = {'user-agent':
+                              'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'}
+
+    baseURL_playerstat = 'http://stats.nba.com/stats/leaguedashplayerstats?'
+
+    parameters_playerstat = {
+        'College': '',
+        'Conference': '',
+        'Country': '',
+        'DateFrom': '',
+        'DateTo': '',
+        'Division': '',
+        'DraftPick': '',
+        'DraftYear': '',
+        'GameScope': '',
+        'GameSegment': '',
+        'Height': '',
+        'LastNGames': 0,
+        'LeagueID': '00',
+        'Location': '',
+        'MeasureType': 'Base',
+        'Month': 0,
+        'OpponentTeamID': 0,
+        'Outcome': '',
+        'PORound': 0,
+        'PaceAdjust': 'N',
+        'PerMode': 'PerGame',
+        'Period': 0,
+        'PlayerExperience': '',
+        'PlayerPosition': '',
+        'PlusMinus': 'N',
+        'Rank': 'N',
+        'Season': '2016-17',
+        'SeasonSegment': '',
+        'SeasonType': 'Regular Season',
+        'ShotClockRange': '',
+        'StarterBench': '',
+        'TeamID': 0,
+        'VsConference': '',
+        'VsDivision': '',
+        'Weight': '',
+    }
+
+    response = requests.get(baseURL_playerstat, params=parameters_playerstat, headers=user_agent_headers)
+    response.raise_for_status()
+    headers = response.json()['resultSets'][0]['headers']
+    stats = response.json()['resultSets'][0]['rowSet']
+
+    #print(json.dumps(stats, indent=4, sort_keys=True))
+
+    stats_df = pandas.DataFrame(stats, columns=headers)
+    stats_df['Season'] = parameters_playerstat['Season']
+    stats_df.drop(['CFID', 'CFPARAMS'], axis=1, inplace=True)
+    return stats_df
+
+
+if __name__ == '__main__':
+    main()
